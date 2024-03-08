b9401dfaf364ddbecd2fa4de3cd7076c7eb9b83a
gumeeee <gaxco123@gmail.com>
2024-03-07 21:22:31 -0300

    Commit com a atividade certa, no commit passado não foi tudo, nao sei por que

--- src/main/java/com/helloWorld/helloWorld/controller/HelloWorldController.java
+++ src/main/java/com/helloWorld/helloWorld/controller/HelloWorldController.java
@@ -5,11 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello-world")
+@RequestMapping
 public class HelloWorldController {
-	
-	@GetMapping
+
+	@GetMapping("hello-world")
 	public String helloWorld() {
 		return "Hello World!!!";
+	}
+
+	@GetMapping("/bsms")
+	public String bsm() {
+		return "Habilidades: Trabalho em equipe, Orientação aos detalhes, Proatividade, Comunicação; \n Mentalidades: Persistência, Responsabilidade Pessoal, Orientação ao futuro, mentalidade de crescimento";
+	}
+
+	@GetMapping("/objetivos")
+	public String objetivos() {
+		return "Objetivo é concluir estudo de módulos da faculdade, compreender tudo sem duvidas sobre spring e melhorar meus métodos de estudos!";
 	}
 }
